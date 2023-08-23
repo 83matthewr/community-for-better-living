b9e2b97dddb1f2fdcbcfe4fcb43279c845f4a747
Matthew <83matthewr@gmail.com>
2023-08-23 16:46:44 -0400

    Added mobile help our cause section

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,15 @@
+import Image from "next/image"
+
 import MobileHero from "../components/mobile-hero.component"
 import brownBags from "../../public/assets/brownbags.jpg"
+import donation from '../../public/assets/donation.jpg'
 
 export default function Home() {
   return (
     <div>
-      <MobileHero image={brownBags}/>
+      <MobileHero image={brownBags} />
       <div className="container mx-auto">
+
         <div className="p-8">
           <h2 className="display-s text-primary-900 mb-4">Making a difference</h2>
           <p className="body-2s text-neutral-900">
@@ -18,6 +22,7 @@ export default function Home() {
             Our Phone Number is (440)541-7829 (please be patient as we are updating our number)
           </p>
         </div>
+
         <div className="px-8 pb-8">
           <h2 className="display-s text-primary-900 mb-4">Services</h2>
           <p className="body-2s text-neutral-900">
@@ -25,6 +30,36 @@ export default function Home() {
             Turpis adipiscing accumsan aenean ut. In etiam sit mauris id. Quam venenatis enim pellentesque integer.
           </p>
         </div>
+
+        <div className="px-8 pb-8">
+          <h2 className="display-s text-primary-900 mb-4">Help Our Cause</h2>
+          <div className="w-full bg-primary-100 rounded-md shadow-md">
+            <div className="w-full">
+              <Image
+                alt="Mountains"
+                src={donation}
+                sizes="100vw"
+                // Make the image display full width
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                  borderRadius: '0.375rem 0.375rem 0 0'
+                }}
+              />
+            </div>
+            <div className="p-4">
+              <p className="body-3r text-neutral-900 mb-4">
+                If you can, please help us as we provide groceries and healthy living education free of charge to families
+                in need across Northeast Ohio. Since the COVID-19 pandemic began in the spring of 2020, we have provided
+                food to over 1000 families and continue to increase those numbers with your support.
+              </p>
+              <button className="w-full py-2 rounded bg-primary-900 text-white display-5">
+                Support Us
+              </button>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   )
