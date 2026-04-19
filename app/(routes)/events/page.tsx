79b08bda144e8e03afd5bd6ba2d494427230d70c
Matthew Roberts <mroberts@baypointetech.com>
2026-04-19 19:36:07 -0400

    Added Bingo Night event

--- app/(routes)/events/page.tsx
+++ app/(routes)/events/page.tsx
@@ -15,6 +15,7 @@ import carshowImage from "../../../public/assets/car_show_event.jpg";
 import ChristmasEventImage from "../../../public/assets/2025christmas.jpg";
 import comingSoonImage from "../../../public/assets/coming_soon.jpg";
 import luncheonImage from "../../../public/assets/luncheon_event.jpg";
+import BingoEventImage from "../../../public/assets/bingo.jpg";
 import Link from "next/link";
 
 const EVENTS_DATA = [
@@ -93,57 +94,16 @@ export default function Events() {
               )
             }) */}
 
-            <ImageCard image={luncheonImage} height="300px" alt="">
-              <div className="2xl:h-[620px] flex flex-col gap-4 justify-between p-0 xs:p-2 lg:p-4">
+            <ImageCard image={BingoEventImage} height="300px" alt="">
+              <div className="2xl:h-[480px] flex flex-col gap-4 justify-between p-0 xs:p-2 lg:p-4">
                 <div className="flex flex-col gap-4">
                   <div>
-                    <h3 className="display-2 text-primary-900">2nd Annual Grateful Women&apos;s Luncheon</h3>
+                    <h3 className="display-2 text-primary-900">Bingo Night</h3>
                   </div>
                   <div className="flex flex-col gap-1">
                     <h4 className="display-4 text-neutral-900">Date:</h4>
-                    <p className="body-1r text-neutral-900">Saturday, November 8, 2025</p>
-                    <p className="body-1r text-neutral-900">11:00 a.m. - 2:00 p.m.</p>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <h4 className="display-4 text-neutral-900">Location:</h4>
-                    <p className="body-1r text-neutral-900">Hosted by Matteo&apos;s Italian Restaurant</p>
-                    <p className="body-1r text-neutral-900">8072 Columbia Rd</p>
-                    <p className="body-1r text-neutral-900">Olmsted Falls, OH 44138</p>
-                  </div>
-                  <div className="flex flex-col gap-4">
-                    <p className="body-1r text neutral-900">
-                      Join us for a lunch buffet, dessert, beverages, a guest speaker, and raffles, all while helping raise
-                      funds for The Community for Better Living&apos; Food Pantry and classes.
-                    </p>
-                    <p className="body-1r text neutral-900 mb-4">
-                      $50/per Plate and all proceeds go directly to the Community for Better Living&apos;s Food Pantry. Please
-                      register and pay ahead of time to reserve your seat!
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-col gap-4">
-                  <Link
-                    href="https://portal.dubsado.com/public/form/view/66a56fb579f4c3003a4b2e0d?"
-                    className="w-full py-2 bg-primary-900 text-primary-100 body-2b md:body-1b lg:body-2b xl:body-1b text-center rounded-md"
-                  >
-                    Register
-                  </Link>
-                  <Link
-                    href="https://www.paypal.com/ncp/payment/8NRH7GBX2T97G"
-                    className="w-full py-2 bg-primary-900 text-primary-100 body-2b md:body-1b lg:body-2b xl:body-1b text-center rounded-md"
-                  >
-                    Pay
-                  </Link>
-                </div>
-
-              </div>
-            </ImageCard>
-
-            <ImageCard image={ChristmasEventImage} height="300px" alt="">
-              <div className="2xl:h-[620px] flex flex-col gap-4 justify-between p-0 xs:p-2 lg:p-4">
-                <div className="flex flex-col gap-4">
-                  <div>
-                    <h3 className="display-2 text-primary-900">5th Annual Adopt-a-Family</h3>
+                    <p className="body-1r text-neutral-900">Saturday, May 30, 2026</p>
+                    <p className="body-1r text-neutral-900">6:00 - 8:00pm (Doors open at 5:30pm)</p>
                   </div>
                   <div className="flex flex-col gap-1">
                     <h4 className="display-4 text-neutral-900">Location:</h4>
@@ -152,50 +112,28 @@ export default function Events() {
                   </div>
                   <div className="flex flex-col gap-4">
                     <p className="body-1r text neutral-900">
-                      The Community for Better Living is seeking sponsors for our
-                      5th annual Adopt-a-Family program. As a non-profit that serves the local community,
-                      we are able to identify families in need during the holidays.
-                      We primarily server the westside of Cleveland and have a client base
-                      that includes Olmsted Falls/Township, Columbia Station, North Olmsted,
-                      Westlake, Lakewood, Bay Village, Rocky River, Berea, Middleburg Heights,
-                      Strongsville, Parma, and beyond.
+                      Come join us for our family friendly Bingo Night Fundraiser! We will have prizes, raffle baskets, 
+                      popcorn, and drinks. As always, it&apos;s FREE to play! All proceeds go directly to the Community for Better 
+                      Living Food Bank. Bring your own dabbers, if you have them, and your game face!
                     </p>
-                    <p className="body-1r text neutral-900 xl:py-6">
-                      Help bring joy to local familiies this holiday!
-                      By sponsoring a family, you&apos;ll provide them with gifts to make their holiday season
-                      brighter. Your generosity can make a real difference in someone&apos;s life!
-                      To learn more of to become a sponsor, please click the registration link below:
+                    <p className="body-1r text neutral-900 mb-4">
+                      If you&apos;d like to donate a raffle basket, gift card, game prize, or sponsor a basket or prize, 
+                      please reach out and let us know!
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <Link
-                    href="http://bit.ly/4oocJel"
-                    className="w-full py-2 bg-primary-900 text-primary-100 body-2b md:body-1b lg:body-2b xl:body-1b text-center rounded-md"
-                  >
-                    Register
-                  </Link>
-                  <Link
-                    href="https://www.paypal.com/ncp/payment/GMP8PPZ7FA2MN"
-                    className="w-full py-2 bg-primary-900 text-primary-100 body-2b md:body-1b lg:body-2b xl:body-1b text-center rounded-md"
-                  >
-                    Donate
-                  </Link>
-                </div>
-
               </div>
             </ImageCard>
 
-            {/*  
             <ImageCard image={comingSoonImage} height="300px" alt="">
-              <div className="2xl:h-[600px] px-0 py-8 lg:py-48 xs:px-2 lg:px-4 text-center">
+              <div className="2xl:h-[480px] px-0 py-8 lg:py-48 xs:px-2 lg:px-4 text-center">
                 <div>
                   <h3 className="display-2 text-primary-900 mb-4">More Events Coming Soon...</h3>
                   <p className="body-1b text-neutral-700">Check back later for more info</p>
                 </div>
               </div>
             </ImageCard>
-            */}
+  
 
             { /*(EVENTS_DATA.length % 2 === 1) && (
               <ImageCard image={comingSoonImage} height="300px" alt="">
